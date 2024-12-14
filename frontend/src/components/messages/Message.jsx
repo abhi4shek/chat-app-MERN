3ee89e269edc68c2ded2f1e10a244fcbff393dd4
abhi4shek <aya913356@gmail.com>
2024-12-15 01:21:27 +0530

    Deployment

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -5,11 +5,11 @@ import useConversation from "../../zustand/useConversation";
 const Message = ({ message }) => {
 	const { authUser } = useAuthContext();
 	const { selectedConversation } = useConversation();
-	const fromMe = message.senderId === authUser._id;
+	const fromMe = message.senderID === authUser._id;
 	const formattedTime = extractTime(message.createdAt);
 	const chatClassName = fromMe ? "chat-end" : "chat-start";
-	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
-	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation.profilePic;
+	const bubbleBgColor = fromMe ? "bg-info" : "";
 
 	const shakeClass = message.shouldShake ? "shake" : "";
 
