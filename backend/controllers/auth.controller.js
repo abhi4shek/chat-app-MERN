d37013c058a4e1a66e0487bc58cdd8234561d89d
abhi4shek <aya913356@gmail.com>
2024-12-14 09:06:08 +0530

    Deployment

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -57,8 +57,8 @@ export const signup = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const boyProfilePic = ``;
-    const girlProfilePic = ``;
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
     const newUser = new User({
       fullName,
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,13 +9,14 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-
-
-const app = express();
-const PORT = process.env.PORT || 5500 ;
 
 dotenv.config();
+
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 5500 ;
 
 app.use(express.json()); //to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
@@ -23,15 +25,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) =>{
-//     //root route http://localhost:5500/
-//     res.send("Hello, World!");
-// });
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*", (req, res) =>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+});
 
 
-
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`server running on port ${PORT}`)
 });
