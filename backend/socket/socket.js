b5f7ac6c7e49f443d27de577ad3463aa2a11e4f4
abhi4shek <154368478+abhi4shek@users.noreply.github.com>
2025-03-20 04:25:29 +0530

    socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,8 +7,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["https://chat-app-mern-nawb.onrender.com"],
-		methods: ["GET", "POST"],
+	origin: "*",  // Allow all origins
+	methods: ["GET", "POST"],
+	credentials: true, 
 	},
 });
 
